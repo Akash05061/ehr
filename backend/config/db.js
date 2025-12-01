820214bb90865fb869e74cc9f7749e06c10c35b0
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-01 19:48:56 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,14 @@
+// backend/config/db.js
 const mysql = require("mysql2/promise");
 
-let pool;
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-async function connectDB() {
-  if (!pool) {
-    pool = mysql.createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-    });
-
-    console.log("✅ Connected to RDS MySQL");
-  }
-
-  return pool;
-}
-
-module.exports = connectDB;
+module.exports = pool;
