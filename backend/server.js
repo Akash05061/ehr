aadfa6279789b4dd9bb4e066b0c983f8d95f70c1
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-02 09:27:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,17 @@ const s3Service = require("./s3-service");
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://65.0.169.14",
+    "http://65.0.169.14:3000"
+  ],
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 
 const PORT = process.env.PORT || 3001;
 const JWT_SECRET = process.env.JWT_SECRET;
